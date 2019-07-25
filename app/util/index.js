3ea50cfab16b92d6366a37867f0ee477835fc7fb
abhishekwl <abhishekwl@protonmail.com>
2019-07-25 09:14:55 +0530

    Refactored util and updated env variables

--- app/util/index.js
+++ app/util/index.js
@@ -1,7 +1,8 @@
 function setupDatabase() {
     const mongoose = require('mongoose');
     mongoose.Promise = global.Promise;
-    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useFindAndModify: false })
+    const dbUri = 'mongodb+srv://'+process.env.DB_USER_NAME+':'+process.env.DB_USER_PASSWORD+'@'+process.env.DB_HOST+'?retryWrites=true&w=majority'
+    mongoose.connect(dbUri, { useNewUrlParser: true, useFindAndModify: false })
         .then(() => console.log('[DB] Connection success'))
         .catch(err => { console.log('[!DB] Connection failed\n'+err); process.exit(); });
 };
